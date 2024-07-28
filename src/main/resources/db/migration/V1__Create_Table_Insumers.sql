0888c47b3eb6f06298f14eba33be4d2f7a53bbd3
Danilo Ramon <daniloramondesenvolvedor@gmail.com>
2024-07-27 23:29:14 -0300

    build: retirando flyway e dependências, adicionando ddl-auto:update no application.yml

--- src/main/resources/db/migration/V1__Create_Table_Insumers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE insumers (
-    id SERIAL PRIMARY KEY ,
-    preco DOUBLE PRECISION not null,
-    nome VARCHAR(255) NOT NULL,
-    medida VARCHAR(5) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    quantidade INTEGER NOT NULL
-
-);
