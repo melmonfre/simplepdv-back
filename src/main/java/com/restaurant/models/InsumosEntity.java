89ff5236fd909d3e0b7555eb08c8c9c07441449c
raqlzz <raqlzz03@gmail.com>
2024-08-17 17:48:12 -0300

    feat:crud do servicos produtos e insumos

--- src/main/java/com/restaurant/models/InsumosEntity.java
+++ src/main/java/com/restaurant/models/InsumosEntity.java
@@ -1,5 +1,6 @@
 package com.restaurant.models;
 
+import com.restaurant.dtos.DetalheInsumo;
 import com.restaurant.models.Enums.Categoria;
 import com.restaurant.models.Enums.Medida;
 import jakarta.persistence.*;
@@ -22,5 +23,26 @@ public class InsumosEntity {
     @Enumerated(EnumType.STRING)
     private Categoria categoria;
     private Integer quantidade;
+
+    public void alteraInsumo (DetalheInsumo dto){
+        if (dto.restaurante() != null){
+            this.restaurante = dto.restaurante();
+        }
+        if (dto.preco() != null){
+            this.preco = dto.preco();
+        }
+        if (dto.nome() != null){
+            this.nome = dto.nome();
+        }
+        if (dto.medida() != null){
+            this.medida = dto.medida();
+        }
+        if (dto.categoria() != null){
+            this.categoria = dto.categoria();
+        }
+        if (dto.quantidade() != null){
+            this.quantidade =  dto.quantidade();
+        }
+    }
 
 }
